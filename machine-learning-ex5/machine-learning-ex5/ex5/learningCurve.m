ca6dc6b690ddc9ad47447b24f7b18327f09b9662
Darren Sun <sunzhe_c2702@163.com>
2016-10-25 18:22:41 +0800

    Finish week6 home assignment
    PLEASE remember that when we are calculating the error J, we should
    set lambda = 0;
    We only use lambda in gradient algorithm for CostValue J and gradium.

--- machine-learning-ex5/machine-learning-ex5/ex5/learningCurve.m
+++ machine-learning-ex5/machine-learning-ex5/ex5/learningCurve.m
@@ -21,6 +21,16 @@ m = size(X, 1);
 error_train = zeros(m, 1);
 error_val   = zeros(m, 1);
 
+for i = 1:m
+   theta = trainLinearReg(X(1:i,:), y(1:i), lambda);
+   %Note the lambda should be 0 for the error
+   Jtrain = linearRegCostFunction(X(1:i,:), y(1:i), theta, 0); 
+   Jval = linearRegCostFunction(Xval,yval, theta, 0);
+   error_train(i) = Jtrain;
+   error_val(i) = Jval;
+end
+
+
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return training errors in 
 %               error_train and the cross validation errors in error_val. 
--- machine-learning-ex5/machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -19,16 +19,9 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
+J = 1/(2*m)*((X*theta - y)'*(X*theta-y)) + lambda/(2*m)*(theta(2:end)'*theta(2:end));
 
-
-
-
-
-
-
-
-
-
+grad = 1/m*X' * (X*theta-y) + lambda/m*([0;theta(2:end)]);
 
 % =========================================================================
 
